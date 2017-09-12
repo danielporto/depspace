c1993931897387fe16b12d7f218c05966ae0cbad
Daniel Porto <daniel.porto@gmail.com>
2017-09-12 15:16:35 +0100

    Changes to make depspace run in docker containers

--- scripts/clientEDS.sh
+++ scripts/clientEDS.sh
@@ -14,4 +14,9 @@ EXT_DIR=$3
 DIR=$(cd `dirname $0` && pwd)
 cd $DIR/..
 
-java -cp bin:lib/SMaRt-eds.jar:lib/PVSS.jar:lib/commons-codec-1.5.jar:lib/core-0.1.4.jar:lib/groovy-1.0-JSR-06.jar:lib/netty-all-4.0.25.Final.jar:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar depspace.demo.EDSDemoClient $CLIENT_ID $CFG_DIR $EXT_DIR
+CLASSPATH=bin/depspace.jar
+CLASSPATH=$CLASSPATH:lib/SMaRt-eds.jar:lib/PVSS.jar:lib/commons-codec-1.5.jar
+CLASSPATH=$CLASSPATH:lib/core-0.1.4.jar:lib/groovy-1.0-JSR-06.jar:lib/netty-all-4.0.25.Final.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-api-1.5.8.jar:lib/slf4j-jdk14-1.5.8.jar
+
+java -cp $CLASSPATH depspace.demo.EDSDemoClient $CLIENT_ID $CFG_DIR $EXT_DIR
